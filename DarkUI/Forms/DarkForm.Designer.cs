8dbd01b64ba0daff6b00c4c0c6491eab92496959
Craig Shaw <44495972+Shawminator@users.noreply.github.com>
2025-04-22 16:42:33 +0100

    Delete DarkUI directory

--- DarkUI/Forms/DarkForm.Designer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DarkUI.Forms
-{
-	// Token: 0x02000012 RID: 18
-	public partial class DarkForm : global::System.Windows.Forms.Form
-	{
-	}
-}
--- DarkUI/Forms/DarkTranslucentForm.Designer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DarkUI.Forms
-{
-	// Token: 0x02000015 RID: 21
-	internal partial class DarkTranslucentForm : global::System.Windows.Forms.Form
-	{
-	}
-}
